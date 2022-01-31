b1c99a9d47012cfd53102dd3f50545869f13864a
PeterAAyala <peteraayala@gmail.com>
2022-01-30 23:04:41 -0800

    Update HeaderLink

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,10 +1,11 @@
 
-function HeaderLink({Icon, text}) {
+function HeaderLink({ Icon, text, avatar }) {
     
     return (
         <div>
-        <Icon className="!h-7 !w-7 lg:!-mb-1" />
-        {text}
+            
+            <Icon className="!h-7 !w-7 lg:!-mb-1" />
+            {text}
         </div>
     );
 }
