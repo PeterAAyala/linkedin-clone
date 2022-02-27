c120d0842c6a3de34057c0235aade5ebf20abab4
PeterAAyala <peteraayala@gmail.com>
2022-02-27 15:44:54 -0800

    Formatting of header buttons

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,11 +1,16 @@
 
-function HeaderLink({ Icon, text, avatar }) {
+function HeaderLink({ Icon, text, avatar, feed }) {
     
     return (
-        <div>
-            
-            <Icon className="!h-7 !w-7 lg:!-mb-1" />
-            {text}
+        <div 
+            className={`cursor-pointer flex flex-col justify-center items-center ${
+                feed 
+                ? "text-black/50 hover:text-black dark:text-white/70 dark:hover:text-white lg:-mb-1.5 space-y-1" 
+                : "text-gray-500 hover:text-gray-700"
+            }`} 
+        >
+            {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
+            <h4 className="text-sm">{text}</h4>
         </div>
     );
 }
--- pages/home.js
+++ pages/home.js
@@ -15,14 +15,25 @@ function Home() {
                 </div>
                 <div className="flex items-center sm:divide-x divide-gray-300">
                     <div className="hidden sm:flex space-x-8 pr-4">
-                        <HeaderLink Icon={ExploreIcon} text="Discover"/> 
+                        <HeaderLink Icon={ExploreIcon} text="Discover" /> 
                         <HeaderLink Icon={GroupIcon} text="People"/> 
                         <HeaderLink Icon={OnDemandVideoSharpIcon} text="Learning"/> 
                         <HeaderLink Icon={BusinessCenterIcon} text="Jobs"/> 
                     </div>
+                    <div className="pl-4">
+                        <button className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2">
+                            Sign in
+                        </button>
+                    </div>
                 </div>
 
             </header>
+
+            <main>
+
+                
+            </main>
+
         </div>
 
     )
